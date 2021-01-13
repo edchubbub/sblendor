f5dfa973ba1ccc376b865bed4b0aeaa25cbdcf6e
Elizer Dela Cruz <elizer@hubbub.ai>
2021-01-13 09:58:29 +0800

    Refactored Board Game Server HTTP

--- src/main/scala/com/sblendor/BoardGameRoutes.scala
+++ src/main/scala/com/sblendor/BoardGameRoutes.scala
@@ -19,31 +19,34 @@ final case class GetGemsRequest(gems: List[Gem])
 
 trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
 
-  implicit val getGemsRequestFormatx: RootJsonFormat[GetGemsRequest] = new RootJsonFormat[GetGemsRequest] {
+  implicit val getGemsRequestFormat: RootJsonFormat[GetGemsRequest] = new RootJsonFormat[GetGemsRequest] {
     def write(request: GetGemsRequest): JsValue = {
       JsArray(request.gems.map {
-        case Gem.Diamond => JsString("diamond")
-        case Gem.Emerald => JsString("emerald")
+        case Gem.Diamond  => JsString("diamond")
+        case Gem.Emerald  => JsString("emerald")
         case Gem.Sapphire => JsString("sapphire")
-        case Gem.Opal => JsString("opal")
-        case Gem.Ruby => JsString("ruby")
+        case Gem.Opal     => JsString("opal")
+        case Gem.Ruby     => JsString("ruby")
       }.toVector)
     }
 
     def read(json: JsValue): GetGemsRequest = {
-      val xxx = json.asJsObject.getFields("gems").map {
-        case JsString(name) =>
-          name.toLowerCase match {
-            case "diamond" => Gem.Diamond
-            case "emerald" => Gem.Emerald
-            case "sapphire" => Gem.Sapphire
-            case "opal" => Gem.Opal
-            case "ruby" => Gem.Ruby
-            case _ => throw DeserializationException("Invalid Gem.")
+      val gems = json.asJsObject.getFields("gems") match {
+        case Seq(JsArray(elements)) =>
+          elements.map {
+            case JsString(name) =>
+              name.toLowerCase match {
+                case "diamond"  => Gem.Diamond
+                case "emerald"  => Gem.Emerald
+                case "sapphire" => Gem.Sapphire
+                case "opal"     => Gem.Opal
+                case "ruby"     => Gem.Ruby
+                case _          => throw DeserializationException("Invalid Gem.")
+              }
           }
-        case _ => throw DeserializationException("JSON Gem parsing failed.")
+        case _ => throw DeserializationException(s"JSON Gem parsing failed.")
       }
-      GetGemsRequest(xxx.toList)
+      GetGemsRequest(gems.toList)
     }
   }
 }
